b4064876846658faea9b68fe7956cfb1fbd37a3b
paulforde <128531322+paulforde@users.noreply.github.com>
2026-06-10 17:37:41 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = "quiz-challenge-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req)
+        .then((res) => {
+          const url = new URL(req.url);
+          const isFlag = /flagcdn\.com|flagicons\.lipis\.dev|hatscripts\.github\.io/.test(url.hostname);
+          if ((url.origin === location.origin && res.ok) || isFlag) {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy));
+          }
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
